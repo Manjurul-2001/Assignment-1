41b94c2a70a072cab37534c07a5685e3a6d8af1b
Md. Manjurul Islam <manjurul.apmat.du@gmail.com>
2025-03-02 13:48:23 +0600

    Update A1Q4.py

--- A1Q4.py
+++ A1Q4.py
@@ -1,1 +1,18 @@
-print("Hello world!")
+
+file=open("mbox.txt")
+count=0
+total_confidence=0
+for line in file:
+    line=line.strip()
+    if line.startswith('X-DSPAM-Confidence:'):
+        print(line)
+        words=line.split()
+        confidence=float(words[1])
+        total_confidence=total_confidence+confidence
+        count=count+1
+print(count)
+if count>0:
+    avg=total_confidence/count
+    print("Average sum", avg)
+else:
+    print("NO file is X-DSPAM-Confidence found")
